18aaaa6b5caebd1a77216bee69a45cc0b1c23718
seena127 <45530763+seena127@users.noreply.github.com>
2024-10-16 20:05:44 +0530

    Update cpu_mem_monitoring.sh

--- cpu_mem_monitoring.sh
+++ cpu_mem_monitoring.sh
@@ -5,8 +5,9 @@
 #Notes: System monitoring based shell scripting
 #
 practice(){
-if [ $# -ne 2 ]; then
-        echo "please provide cpu and memory threshold arguments as variables in format: ./alerting.sh cpu_threshold memory_threshold"
+if [ $# -ne 3 ]; then
+        echo "please provide cpu and memory threshold arguments as variables in format: ./alerting.sh cpu_threshold memory_threshold disk_space_threshold"
+        exit 1
 else
         echo "performing monitoring for the system based on threshold value"
 fi
@@ -15,11 +16,12 @@ practice "$@"
 
 CPU_Threshold=$1
 Memory_Threshold=$2
+Disk_needed=$3
 
 #calculating current memory and cpu usage in machine
 Mem_usage=$(free |awk '/Mem:/ {print $3/$2 *100}')
 CPU_usage=$(mpstat 1 1|awk '/Average:/ {print 100 - $12}')
-
+Disk_available=$( df -h | awk '$NF=="/"{print 100-$5+0}')
 
 #Function to check cpu and memor usage
 #
@@ -30,8 +32,12 @@ fi
 if(($(echo "$CPU_usage>$CPU_Threshold"|bc -l)));then
         echo "CPU Usage above threshold limit, cpu usage: $Mem_usage"
 fi
+if(($(echo "$Disk_needed>$Disk_available" | bc -l) )); then
+        echo "Disk available is less than needed,, disk available percent: $Disk_usage"
+fi
 echo "CPU usage value: $CPU_usage"
 echo "Memory usage value: $Mem_usage"
+echo "Disk space Available: $Disk_available"
 }
 
 #calling the function
